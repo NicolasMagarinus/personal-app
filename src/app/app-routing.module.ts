f3c7fadb7e8a2ec73dceae8194621df1827c1a51
Nicolas Magarinus Leohnardt <nmleohnardt@gmail.com>
2025-06-09 17:10:21 -0300

    Ajustes gerais

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule) },
-  { path: 'pages/about', loadChildren: () => import('./pages/about-me/about-me.module').then(m => m.AboutMePageModule) },
+  { path: 'pages/about-me', loadChildren: () => import('./pages/about-me/about-me.module').then(m => m.AboutMePageModule) },
   { path: 'pages/projects', loadChildren: () => import('./pages/projects/projects.module').then(m => m.ProjectsPageModule) },
   { path: 'pages/contact', loadChildren: () => import('./pages/contact/contact.module').then(m => m.ContactPageModule) },
   { path: 'pages/skills', loadChildren: () => import('./pages/skills/skills.module').then(m => m.SkillsPageModule) },
